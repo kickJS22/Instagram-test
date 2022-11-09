99f5844e6b41f59bf6d7f2fa2176dc0c48d3ac42
kickJS22 <115955099+kickJS22@users.noreply.github.com>
2022-11-08 21:55:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let getEmail = document.getElementById("#email");
+let getPass = document.getElementById("#pass");
+let btn = document.getElementById("#init");
+
+// btn.addEventListener("click", function getInfo(){
+//     database.ref('/mail%s',getEmail.value).set({
+//         email: getEmail.value,
+//         pass: getPass.value
+//     });
+//     console.log(getEmail.value);
+//     console.log(getPass.value);
+// });
