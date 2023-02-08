20c5eaa707c4aee5055d48ddd6f0174b59a43e40
kickJS22 <bustillosjuancho+github@gmail.com>
2023-02-08 13:40:18 -0300

    fixing form, queda db

---
+++ database.js
@@ -0,0 +1,5 @@
+
+var sqlite3 = require('sqlite3');
+
+const db = sqlite3.Database("account.db");
+
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-let getEmail = document.getElementById("#email");
-let getPass = document.getElementById("#pass");
-let btn = document.getElementById("#init");
 
-// btn.addEventListener("click", function getInfo(){
-//     database.ref('/mail%s',getEmail.value).set({
-//         email: getEmail.value,
-//         pass: getPass.value
-//     });
-//     console.log(getEmail.value);
-//     console.log(getPass.value);
-// });
+const form = document.querySelector("form");
+const btn = document.getElementById("#init");
+const mail = document.querySelectorAll("input")[0];
+const pass = document.querySelectorAll("input")[1];
+
+function getInfo(){
+    console.log("Dom loaded");
+    btn.disabled = true;
+    btn.style.opacity = "35%";
+}
+
+document.addEventListener("DOMContentLoaded", getInfo())
+
+
+form.onchange = () => {
+    if (mail.value != "" && pass.value != ""){
+        btn.disabled = false;
+        btn.style.opacity = "100%";
+        console.log(mail.value, pass.value)
+
+    } else {
+        btn.disabled = true;
+        btn.style.opacity = "35%";
+    }
+}
+
